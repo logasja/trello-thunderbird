1c6795dafcc35724c74ba678d0ca6cf376af2152
Jacob Logas <logasja@outlook.com>
2021-09-26 11:49:12 -0400

    Getting token and saving to storage

--- src/options.ts
+++ src/options.ts
@@ -1,49 +1,41 @@
 import '../html/options.html.src';
+import {TrelloClient} from 'trello.js';
 
 async function auth() {
   let key = process.env.TRELLO_KEY;
-  // let width = 420;
-  // let height = 470;
-  // let left = window.screenX + (window.innerWidth - width) / 2;
-  // let top = window.screenY + (window.innerHeight - height) / 2;
-  let origin = browser.extension.getBackgroundPage().location + '/options.html';
-  var token;
+  let appName = process.env.APP_NAME;
+  // let origin = browser.extension.getBackgroundPage().location;
 
-  console.log(origin);
-
-  let authUrl = `https://trello.com/1/authorize?return_url=${origin}&callback_method=postMessage&expiration=never&name=Project&key=${key}`
+  let authUrl = `https://trello.com/1/authorize?expiration=never&name=${appName}&key=${key}`
+  // let authUrl = `https://trello.com/1/authorize?return_url=${origin}&callback_method=postMessage&expiration=never&name=Project&key=${key}`
 
   browser.windows.create({
-    // type: 'popup',
+    type: 'popup',
     url: authUrl,
-    // width: width,
-    // height: height,
-    // left: left,
-    // top: top,
     allowScriptsToClose: true
   });
 
-  var receiveMessage = function(event: any) {
-    var ref2;
-    console.log(event);
-    if ((ref2 = event.source) != null) {
-      ref2.close();
-    }
-    if ((event.data != null) && /[0-9a-f]{64}/.test(event.data)) {
-      //Send Trello token to rails server through $resource model.
-      token = event.data;
-      console.log(token);
-    } else {
-      token = null;
-    }
-    if (typeof window.removeEventListener === 'function') {
-      //remove event listener
-      window.removeEventListener('message', receiveMessage, false);
-    }
-  };
-  window.addEventListener('message', receiveMessage, false);
-  // return typeof window.addEventListener === 'function' ? window.addEventListener('message', receiveMessage, false) : void 0;
+  document.getElementById('authbutton')!.classList.add('hidden');
+  document.getElementById('token-fieldset')!.classList.remove('hidden');
+  document.getElementById('auth-form')!.onsubmit = saveToken;
 }
+
+function saveToken(event: Event) {
+  console.log(event);
+  let form = document.getElementById('auth-form') as HTMLFormElement;
+  let data = new FormData(form);
+  for (const [name, value] of data) {
+    console.log(name, value);
+    // @ts-expect-error
+    browser.storage.local.set({name, value});
+  }
+  form.onsubmit = null;
+  form.classList.add('hidden');
+}
+
+// async function showTrelloAccount() {
+//   let client = new TrelloClient({key: })
+// }
 
 function main() {
   document.getElementById('authbutton')!.onclick = auth;
